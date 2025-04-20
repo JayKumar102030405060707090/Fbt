04ba4a845cbe35ebafe085dd0677d425b1134905
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-20 10:33:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from utils.logger import logger
 
 bot = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
+async def main():
+    await bot.start()
+    await setup_handlers(bot)
+    logger.info(f"Bot started as @{(await bot.get_me()).username}")
+    await bot.idle()
+
 if __name__ == "__main__":
-    setup_handlers(bot)
-    logger.info("Bot is starting...")
-    bot.run()
+    import asyncio
+    asyncio.run(main())
