bf0a51e005ab60437b47b51718bebd57d69c3602
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-04-20 10:28:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+OWNER_ID = int(os.getenv("OWNER_ID", "7168729089"))
 API_ID = int(os.getenv("API_ID", 20882859))
 API_HASH = os.getenv("API_HASH", "7c8f0bd8aa7be4af2a02492f9a807778")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7131160767:AAF220cVpiwmsnIgQly9nGBaaZiqokgNWzc")
